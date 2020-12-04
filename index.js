516bd855bb1242a2c3dd8cfbb09599d1a6c8d248
rlziesch <74370357+rlziesch@users.noreply.github.com>
2020-12-04 14:29:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+/* Decide My Day Script */
+
+function turnArray() {
+  //assign value of textarea to mytext
+  var outfitText = document.getElementById("outfitoptions").value;
+
+  //split mytext by nextline = "\n" 
+  var outfitArray = outfitText.split("\n");
+
+/* add second */
+
+  var mealsText = document.getElementById("mealoptions").value;
+
+  var mealsArray = mealsText.split("\n");
+
+/* add third */
+
+  var tasksText = document.getElementById("taskoptions").value;
+
+  var tasksArray = tasksText.split("\n");
+
+/* start picking random */
+
+const randomOutfit = outfitArray[Math.floor(Math.random() * outfitArray.length)];
+
+const randomMeal = mealsArray[Math.floor(Math.random() * mealsArray.length)];
+
+const randomTask = tasksArray[Math.floor(Math.random() * tasksArray.length)];
+
+/* do i need listener 
+
+textarea.addEventListener('blur', saveNames, false);
+
+*/
+
+/* get result */
+
+document.getElementById("result").innerHTML = 'Today you will wear ' + randomOutfit + '. ' + 'Plan on eating ' + randomMeal + ' later.' + ' Your assigned task to accomplish is ' + randomTask + ' so make sure you plan accordingly!';
+}
+
+/* end decide my day script */
+
+
 /* responsive navigation */
 
 function myFunction() {
@@ -8,7 +51,6 @@ function myFunction() {
       x.className = "topnav";
     }
   }
-
 
 
   /* add scroll button */
@@ -29,50 +71,6 @@ function myFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
-
-
-
-  /* Decide My Day Script */
-
-  function turnArray() {
-    //assign value of textarea to mytext
-    var outfitText = document.getElementById("outfitoptions").value;
-
-    //split mytext by nextline = "\n" 
-    var outfitArray = outfitText.split("\n");
-
-/* add second */
-
-    var mealsText = document.getElementById("mealoptions").value;
-
-    var mealsArray = mealsText.split("\n");
-  
-  /* add third */
-  
-    var tasksText = document.getElementById("taskoptions").value;
-
-    var tasksArray = tasksText.split("\n");
-
-/* start picking random */
-  
-const randomOutfit = outfitArray[Math.floor(Math.random() * outfitArray.length)];
-
-const randomMeal = mealsArray[Math.floor(Math.random() * mealsArray.length)];
-  
-const randomTask = tasksArray[Math.floor(Math.random() * tasksArray.length)];
-  
- 
-/* get result */
-  
-  document.getElementById("result").innerHTML = 'Today you will wear ' + randomOutfit + '. ' + 'Plan on eating ' + randomMeal + ' later.' + ' Your assigned task to accomplish is ' + randomTask + ' so make sure you plan accordingly!';
-}
-
-/* end decide my day script */
-
-
-
-
-
 
 
 
