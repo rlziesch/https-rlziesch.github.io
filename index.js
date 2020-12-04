50844f5ece22cdb29162d7fdc870fcdcef66e16e
rlziesch <74370357+rlziesch@users.noreply.github.com>
2020-12-04 15:27:25 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,70 +7,27 @@ function turnArray() {
   //split mytext by nextline = "\n" 
   var outfitArray = outfitText.split("\n");
 
-/* add second */
+  /* add second */
 
   var mealsText = document.getElementById("mealoptions").value;
 
   var mealsArray = mealsText.split("\n");
 
-/* add third */
+  /* add third */
 
   var tasksText = document.getElementById("taskoptions").value;
 
   var tasksArray = tasksText.split("\n");
 
-/* start picking random */
+  /* start picking random */
 
-const randomOutfit = outfitArray[Math.floor(Math.random() * outfitArray.length)];
+  const randomOutfit = outfitArray[Math.floor(Math.random() * outfitArray.length)];
 
-const randomMeal = mealsArray[Math.floor(Math.random() * mealsArray.length)];
+  const randomMeal = mealsArray[Math.floor(Math.random() * mealsArray.length)];
 
-const randomTask = tasksArray[Math.floor(Math.random() * tasksArray.length)];
+  const randomTask = tasksArray[Math.floor(Math.random() * tasksArray.length)];
 
-/* do i need listener 
+  /* get result */
 
-textarea.addEventListener('blur', saveNames, false);
-
-*/
-
-/* get result */
-
-document.getElementById("result").innerHTML = 'Today you will wear ' + randomOutfit + '. ' + 'Plan on eating ' + randomMeal + ' later.' + ' Your assigned task to accomplish is ' + randomTask + ' so make sure you plan accordingly!';
+  document.getElementById("result").innerHTML = 'Today you will wear ' + randomOutfit + '. ' + 'Plan on eating ' + randomMeal + ' later.' + ' Your assigned task to accomplish is ' + randomTask + ' so make sure you plan accordingly!';
 }
-
-/* end decide my day script */
-
-
-/* responsive navigation */
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-
-
-  /* add scroll button */
-
-  var mybutton = document.getElementById("myBtn");
-
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
-  }
-
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
-
-
-
